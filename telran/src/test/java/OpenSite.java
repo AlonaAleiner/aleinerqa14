2abaf5f95e9600ab5a7864f33267e05bbe628331
AlonaAleiner <37845299+AlonaAleiner@users.noreply.github.com>
2018-04-29 23:57:06 +0300

    Open telran site

---
+++ telran/src/test/java/OpenSite.java
@@ -0,0 +1,64 @@
+
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.*;
+import static org.testng.Assert.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class OpenSite {
+  private WebDriver driver;
+
+  @BeforeClass(alwaysRun = true)
+  public void setUp() throws Exception {
+    driver = new ChromeDriver();
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+
+  @Test
+  public void openSite() throws Exception {
+    driver.get("https://www.google.co.il/search?q=google&oq=goo&aqs=chrome.0.69i59j69i60j0j69i60l2j69i65.1331j0j1&sourceid=chrome&ie=UTF-8");
+    driver.findElement(By.id("lst-ib")).click();
+    driver.findElement(By.id("lst-ib")).clear();
+    driver.findElement(By.id("lst-ib")).sendKeys("tel");
+    driver.findElement(By.id("lst-ib")).sendKeys(Keys.DOWN);
+    driver.findElement(By.id("lst-ib")).clear();
+    driver.findElement(By.id("lst-ib")).sendKeys("tel ran");
+    driver.findElement(By.id("lst-ib")).sendKeys(Keys.DOWN);
+    driver.findElement(By.id("lst-ib")).sendKeys(Keys.DOWN);
+    driver.findElement(By.id("lst-ib")).clear();
+    driver.findElement(By.id("lst-ib")).sendKeys("tel ran rehovot site");
+    driver.findElement(By.id("lst-ib")).sendKeys(Keys.ENTER);
+    driver.findElement(By.xpath("//div[@id='rso']/div/div/div[2]/div/div/h3/a/span")).click();
+  }
+
+  @AfterClass(alwaysRun = true)
+  public void tearDown() throws Exception {
+    driver.quit();
+
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+
+}
